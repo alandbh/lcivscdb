4a7ae5363f61719c5e655878199a8c8a6c253ddb
Alan Vasconcelos <alandbh@gmail.com>
2024-09-09 22:25:13 -0300

    Improving datepicker in order to keep the most recent selected date

--- components/ui/Datepicker.tsx
+++ components/ui/Datepicker.tsx
@@ -69,6 +69,7 @@ export function Datepicker({
                         closePopover();
                     }}
                     initialFocus
+                    defaultMonth={date}
                 />
             </PopoverContent>
         </Popover>
